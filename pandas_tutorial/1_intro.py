10045a1f04ed2aa90355580b09bda7d856215b09
liuyanan <oceanwave@yeah.net>
2018-12-01 15:53:44 +0800

    add

---
+++ pandas_tutorial/1_intro.py
@@ -0,0 +1,91 @@
+'''
+Created on Dec 1, 2018
+
+@author: liuyanan
+'''
+
+import pandas as pd
+import datetime
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+from unittest.mock import inplace
+
+style.use('ggplot')
+
+
+def get_data_and_show():
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime(2018, 1, 1)
+
+    df = web.DataReader('XOM', "yahoo", start, end)
+
+    print(df)
+    df['Adj Close'].plot()
+
+    plt.show()
+
+
+def panda_basics():
+    # 3个list必须是等长度的
+    web_stats = {'Day': [1, 2, 3, 4, 5, 6],
+                 'Visitors': [45, 67, 34, 78, 90, 44],
+                 'Bounce_Rate': [65, 75, 58, 81, 66, 52]}
+
+    df = pd.DataFrame(web_stats)
+
+    print(df)
+    print(df.head())
+    print(df.tail(1))
+
+    # 原来的dataframe不会变（另外list中的位置不会变）
+    print(df.set_index('Day'))
+
+    # 改变了原来的dataframe
+    df.set_index('Day', inplace=True)
+    print(df)
+    print('\n')
+    print(df['Visitors'])
+
+    print(df[['Bounce_Rate', 'Visitors']])
+
+    print(df.Visitors.tolist())
+
+    print(np.array(df[['Bounce_Rate', 'Visitors']]).tolist())
+
+    df2 = pd.DataFrame(np.array(df[['Bounce_Rate', 'Visitors']]))
+    print(df2)
+
+
+def io_basic():
+    df = pd.read_csv('ZILLOW-Z77006_ZRISFRR.csv')
+    print(df.head())
+    print(df.tail())
+
+    df.set_index('Date', inplace=True)
+    df.to_csv('newcsv2.csv')
+
+    # 在打开的时候指定index列
+    df = pd.read_csv('ZILLOW-Z77006_ZRISFRR.csv', index_col=0)
+    print(df.head())
+
+    df.columns = ['house_price']
+    print(df.head())
+    df.to_csv('newcsv3.csv')
+    df.to_csv('newcsv3_noheader.csv', header=False)
+
+    df = pd.read_csv('newcsv3_noheader.csv', names=['Date', 'HousePrice'], index_col=0)
+    print(df.head())
+
+    df = pd.read_csv('newcsv3.csv')  # , names=['Date', 'HousePrice']) 加上这个参数会有两行header
+    df.rename(index=str, columns={'HousePrice': '77006_price'}, inplace=True)
+    print('\n')
+    print(df.head())
+
+
+if __name__ == '__main__':
+    # get_data_and_show()
+    # panda_basics()
+    # io_basic()
+    io_basic()
---
+++ pandas_tutorial/2_datasset.py
@@ -0,0 +1,100 @@
+'''
+Created on Dec 1, 2018
+
+@author: liuyanan
+'''
+
+import quandl
+import pandas as pd
+from _operator import index
+from unittest.mock import inplace
+
+
+def quandl_data():
+    #     api_key = open('quandlapikey.txt', 'r').read().strip('\n')
+    #     df = quandl.get('FMAC/HPI_AK', authtoken=api_key)
+    #     print(df.head())
+
+    fiddy_states = pd.read_html("https://simple.wikipedia.org/wiki/List_of_U.S._states")
+    print(fiddy_states)
+    for state in fiddy_states:
+        print(state, '\n')
+    print(fiddy_states[0][1])
+
+    for abbv in fiddy_states[0][1][1:]:
+        print('FMAC/HPI_' + abbv)
+
+
+def concatenating_data():
+    df1 = pd.DataFrame({'HPI': [80, 85, 88, 85],
+                        'Int_rate': [2, 3, 2, 2],
+                        'US_GDP_Thounsands': [50, 55, 65, 55]},
+                       index=[2001, 2001, 2003, 2004])
+
+    df2 = pd.DataFrame({'HPI': [80, 85, 88, 85],
+                        'Int_rate': [2, 3, 2, 2],
+                        'US_GDP_Thounsands': [50, 55, 65, 55]},
+                       index=[2005, 2006, 2007, 2008])
+
+    df3 = pd.DataFrame({'HPI': [80, 85, 88, 85],
+                        'Int_rate': [2, 3, 2, 2],
+                        'Low_tier_HPI': [50, 52, 50, 53]},
+                       index=[2001, 2002, 2003, 2004])
+
+    concat = pd.concat([df1, df2])
+    concat = pd.concat([df2, df3])
+    concat = pd.concat([df1, df2, df3])
+    print(concat)
+
+    print(df1.append(df2))
+
+    s = pd.Series([80, 2, 50], index=['HPI', 'Int_rate', 'US_GDP_Thounsands'])
+    df4 = df1.append(s, ignore_index=True)
+    print(df1)
+    print(df4)
+
+
+def joining_and_merging():
+    df1 = pd.DataFrame({'HPI': [80, 85, 88, 85],
+                        'Int_rate': [2, 3, 2, 2],
+                        'US_GDP_Thounsands': [50, 55, 65, 55]},
+                       index=[2001, 2001, 2003, 2004])
+
+    df2 = pd.DataFrame({'HPI': [80, 85, 88, 85],
+                        'Int_rate': [2, 3, 2, 2],
+                        'US_GDP_Thounsands': [50, 55, 65, 55]},
+                       index=[2005, 2006, 2007, 2008])
+
+    df3 = pd.DataFrame({'HPI': [80, 85, 88, 85],
+                        'Unemployemnt': [7, 8, 9, 6],
+                        'Low_tier_HPI': [50, 52, 50, 53]},
+                       index=[2001, 2002, 2003, 2004])
+
+    # on 是共享哪一列
+#     print(pd.merge(df1, df2, on='HPI'))
+#     print(pd.merge(df1, df2, on=['HPI', 'Int_rate']))
+
+    df1.set_index('HPI', inplace=True)
+    df3.set_index('HPI', inplace=True)
+
+    joined = df1.join(df3)
+    print(joined)
+
+    df1 = pd.DataFrame({'Year': [2001, 2002, 2003, 2004],
+                        'Int_rate': [2, 3, 2, 2],
+                        'US_GDP_Thounsands': [50, 55, 65, 55]},
+                       )
+
+    df3 = pd.DataFrame({'Year': [2001, 2003, 2004, 2005],
+                        'Unemployemnt': [7, 8, 9, 6],
+                        'Low_tier_HPI': [50, 52, 50, 53]},
+                       )
+    merged = pd.merge(df1, df3, on='Year', how='outer')
+    merged.set_index('Year', inplace=True)
+    print(merged)
+
+
+if __name__ == '__main__':
+    # quandl_data()
+    # concatenating_data()
+    joining_and_merging()
