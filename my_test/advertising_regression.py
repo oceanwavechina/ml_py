9d71dea484e2b1cd6c17613dfe0c743abe66fe6e
liuyanan <oceanwave@yeah.net>
2018-12-12 23:08:17 +0800

    modify

--- my_test/advertising_regression.py
+++ my_test/advertising_regression.py
@@ -12,23 +12,26 @@ import seaborn as sns
 import matplotlib.ticker as mticker
 
 # 加载数据，指定X，y
-ad_data_all = pd.read_csv("Advertising.csv", index_col=0)
-ad_data = ad_data_all[:-1]
+ad_data = pd.read_csv("Advertising.csv", index_col=0)
+ad_data['forcast'] = np.nan
+
 
 fig = plt.figure(figsize=(12, 10), dpi=80, facecolor='w', edgecolor='k')
+# 原始数据分布
+ax1 = plt.subplot2grid((3, 1), (0, 0), fig=fig)
 
 
 def plot_data():
     # 看看数据是什么样子的, 从图中可以看出，
     #     当费用较小时，报纸和广播的效果还是比较明显的，
     #     但是随着费用的增加，只有电视广告能呈现线性增长的态势
-    ax1 = plt.subplot2grid((3, 1), (0, 0), fig=fig)
-    ax1.plot(ad_data['Sales'], ad_data['TV'], 'ko', markersize=4, label='TV Cost')
-    ax1.plot(ad_data['Sales'], ad_data['Radio'], 'b^', markersize=4, label='Radio Cost')
-    ax1.plot(ad_data['Sales'], ad_data['Newspaper'], 'rx', markersize=4, label='Newspaper Cost')
+    # 因为这个是3个变量的数据，在二维的图中不能呈现所以只要每个变量单独画一下
+    ax1.plot(ad_data['TV'], ad_data['Sales'], 'ko', markersize=4, label='TV Cost')
+    ax1.plot(ad_data['Radio'], ad_data['Sales'], 'b^', markersize=4, label='Radio Cost')
+    ax1.plot(ad_data['Newspaper'], ad_data['Sales'], 'rx', markersize=4, label='Newspaper Cost')
     plt.title('AD_Type & Sales ralation')
-    plt.xlabel('Sales')
-    plt.ylabel('AD Cost')
+    plt.xlabel('AD Cost')
+    plt.ylabel('Sales')
     plt.legend(loc=2)
 
 
@@ -37,9 +40,8 @@ def plot_correlation():
     # 柱子越高则相关性越高
     ax2 = plt.subplot2grid((3, 1), (1, 0), colspan=1, fig=fig)
     df_corr = ad_data.corr()
-    print(df_corr)
-    print(np.array(df_corr['Sales']))
-    col = df_corr['Sales'][:-1]
+    print('corr:\n', df_corr)
+    col = df_corr['Sales'].drop(['forcast', 'Sales'])
     lables = ['TV', 'Radio', 'Newspaper']
     ax2.bar(np.arange(1, len(col) + 1, 1), np.array(col), tick_label=lables, width=0.4)
     plt.title('Sales Correlation')
@@ -49,7 +51,8 @@ def plot_correlation():
 
 def sns_correlation():
     ax3 = plt.subplot2grid((3, 1), (2, 0), fig=fig)
-    sns.heatmap(ad_data.corr(), xticklabels=ad_data.corr().columns, yticklabels=ad_data.corr().columns,
+    corr_data = ad_data[['TV', 'Radio', 'Newspaper']]
+    sns.heatmap(corr_data.corr(), xticklabels=corr_data.corr().columns, yticklabels=corr_data.corr().columns,
                 cmap='RdYlGn', center=0, annot=True, ax=ax3)
 
     ax3.xaxis.set_major_locator(mticker.MaxNLocator(10))
@@ -62,7 +65,7 @@ def plot():
     plot_data()
     plot_correlation()
     sns_correlation()
-    plt.tight_layout()  # 这个是使间距自动适应
+    plt.tight_layout(pad=1)  # 这个是使间距自动适应
     plt.show()
 
 
@@ -78,11 +81,30 @@ def trian_model():
 
     accuracy = clf.score(X_test, y_test)
     print('accuracy: ', accuracy)
+    print('coef:', clf.coef_)
+    print('param:', clf.get_params())
 
-    # 预测一下，看看差多少
+    # 预测 1 条数据，看看差多少
     forcast_sales = clf.predict([X[-1]])
     print('forcast_sales:', forcast_sales)
     print('real_sales:', ad_data[-1:]['Sales'])
+
+    '''
+    学习出来的曲线咋画？
+        好像没法画，因为是多变量的
+
+    # plot
+    ad_data['forcast'] = clf.predict(X)
+    print('data:\n', ad_data.head())
+
+    ax1.plot(ad_data['TV'], ad_data['forcast'], 'yo', markersize=4)
+
+#     radio_range = range(int(ad_data['Radio'].min()), int(ad_data['Radio'].max()), int(ad_data['Radio'].ptp()))
+#     ax1.plot(radio_range, forcast_sales, 'b-')
+
+#     newspaper_range = range(int(ad_data['Newspaper'].min()), int(ad_data['Newspaper'].max()), int(ad_data['Newspaper'].ptp()))
+#     ax1.plot(newspaper_range, forcast_sales[:len(newspaper_range)], 'r-', markersize=4)
+    '''
 
 
 if __name__ == '__main__':
